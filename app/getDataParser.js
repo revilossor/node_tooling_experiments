b7cbf94bfb81433363a6c5e58a0cd061f24abb8a
Oliver <oliver.ross@dwp.gsi.gov.uk>
2016-11-24 17:44:33 +0000

    fixed weird parse undefined error

--- app/getDataParser.js
+++ app/getDataParser.js
@@ -1,8 +1,6 @@
 module.exports = (req, res, next) => {
-  if( (req.method !== "GET")
-  ||  (!req.query || !req.query.data)
-  ||  (typeof req.query.data === 'undefined')) { next(); }
-
-  req.query.data = JSON.parse(req.query.data);
-  next();
+  if(req.method === "GET") {
+    req.query.data = JSON.parse(req.query.data);
+  }
+  return next();
 };

a7e8fceb854c5f892b6b9df1f744b59a045109c2
Oliver <oliver.ross@dwp.gsi.gov.uk>
2016-11-28 11:17:10 +0000

    return appropriate http status

--- app/calcEndpoint.js
+++ app/calcEndpoint.js
@@ -1,6 +1,7 @@
 var router = require('express').Router();
 var response = require('./response');
-var calc = require('./calc.js');
+var calc = require('./calc');
+var error = require('./error');
 /*
   does the specified operation on each member of the passed array in sequence
   ie, 'add', [1, 2, 3, 4] will do (((1+2)+3) + 4)
@@ -11,10 +12,25 @@ function calculate(operation, operands, result) {
     calculate(operation, operands, calc.doOperation(operation, (!result) ? operands.shift() : result, operands.shift()));
 }
 
+function handleRequest(req, res, data) {
+  var returnObject = response.get(calculate(req.params.operation, data));
+  if(returnObject.success === true) {
+    res.status(200).json(returnObject);
+  } else {
+    var status;
+    switch (returnObject.error){
+      case error.invalidArgumentError : status = 400; break;
+      case error.unknownOperationError : status = 404; break;
+      default: status = 500;
+    }
+    res.status(status).json(returnObject);
+  }
+}
+
 router.route('/:operation').get((req, res) => {
-  res.json(response.get(calculate(req.params.operation, req.query.data)));
+  handleRequest(req, res, req.query.data);
 }).post((req, res) => {
-  res.json(response.get(calculate(req.params.operation, req.body.data)));
+  handleRequest(req, res, req.body.data);
 });
 
 module.exports = router;

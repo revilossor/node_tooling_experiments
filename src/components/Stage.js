820c9689e87e98b7ed68fa6998b23b1f719ea235
Oliver <oliver.ross@dwp.gsi.gov.uk>
2016-11-28 15:21:47 +0000

    basic flexboxy view

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './Header';
+import Calculator from './Calculator';
+import Footer from './Footer';
 
 export default class Stage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <Header/>
+        <Calculator/>
+        <Footer/>
       </div>
     );
   }

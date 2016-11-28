3ebd98709109d3e83b70476d6502636828700b25
Oliver <oliver.ross@dwp.gsi.gov.uk>
2016-11-28 16:25:51 +0000

    clic buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 export default class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    console.log('clicky ' + this.props.operation);
+  }
   render() {
     return (
-      <span className="button resetBtn">{this.props.operation}</span>
+      <span className="button resetBtn" onClick={this.handleClick} >{this.props.operation}</span>
     );
   }
 }
--- src/components/ButtonArea.js
+++ src/components/ButtonArea.js
@@ -2,27 +2,30 @@ import React from 'react';
 import Button from './Button';
 
 export default class ButtonArea extends React.Component {
+  // onClick() {
+  //   console.log('clicky on ' + this.props.operation + ' button');
+  // }
   render() {
     return (
       <div className="buttons">
-        <Button operation="AC"/>
-        <Button operation="+/-"/>
-        <Button operation="%"/>
-        <Button operation="/"/>
-        <Button operation="7"/>
-        <Button operation="8"/>
-        <Button operation="9"/>
-        <Button operation="*"/>
-        <Button operation="4"/>
-        <Button operation="5"/>
-        <Button operation="6"/>
-        <Button operation="-"/>
-        <Button operation="1"/>
-        <Button operation="2"/>
-        <Button operation="3"/>
-        <Button operation="+"/>
-        <Button operation="0"/>
-        <Button operation="."/>
+        <Button operation="AC" onClick={this.onClick}/>
+        <Button operation="+/-" onClick={this.onClick}/>
+        <Button operation="%" onClick={this.onClick}/>
+        <Button operation="/" onClick={this.onClick}/>
+        <Button operation="7" onClick={this.onClick}/>
+        <Button operation="8" onClick={this.onClick}/>
+        <Button operation="9" onClick={this.onClick}/>
+        <Button operation="*" onClick={this.onClick}/>
+        <Button operation="4" onClick={this.onClick}/>
+        <Button operation="5" onClick={this.onClick}/>
+        <Button operation="6" onClick={this.onClick}/>
+        <Button operation="-" onClick={this.onClick}/>
+        <Button operation="1" onClick={this.onClick}/>
+        <Button operation="2" onClick={this.onClick}/>
+        <Button operation="3" onClick={this.onClick}/>
+        <Button operation="+" onClick={this.onClick}/>
+        <Button operation="0" onClick={this.onClick}/>
+        <Button operation="." onClick={this.onClick}/>
         <Button operation="???"/>
         <Button operation="="/>
       </div>
